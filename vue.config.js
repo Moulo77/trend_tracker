5ecff444886b67e6258db7e2b45b239586926172
diesetelecom <leo@diesetelecom.com>
2023-11-29 12:55:02 +0100

    i

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,12 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+module.exports = {
+  configureWebpack: {
+    resolve: {
+      fallback: {
+        "path": require.resolve("path-browserify"),
+        "os": require.resolve("os-browserify/browser"),
+        "crypto": require.resolve("crypto-browserify"),
+        "stream": require.resolve("stream-browserify"),
+      },
+    },
+  },
+};
